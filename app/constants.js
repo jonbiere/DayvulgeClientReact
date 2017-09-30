9b7af203b91ef6166f3fa1ab0e6f5773a7b1061d
Jon Biere <jon.biere@gmail.com>
2017-09-30 17:36:23 -0500

    debugging heroku client issues.

--- app/constants.js
+++ app/constants.js
@@ -1,6 +1,6 @@
 //With Reverse Proxy Setup direct traffic same domain to /api
-//export const BaseApiUrl = '';
+export const BaseApiUrl = '';
 
-export const BaseApiUrl = 'https://dev-dayvulge-api.herokuapp.com';
+//export const BaseApiUrl = 'https://dev-dayvulge-api.herokuapp.com';
 //export const BaseApiUrl = 'http://localhost:3001';
 
